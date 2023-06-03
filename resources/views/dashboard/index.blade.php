5197d91f6ffbfe0e7f2250a2206acf02d2ff3174
fahrul <fahrulrozi99321@gmail.com>
2023-06-03 22:19:14 +0700

    finish

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -24,7 +24,7 @@
 
             <!-- Earnings (Monthly) Card Example -->
              @php
-                $total = DB::table('jakpuses')->count();
+                $total = DB::table('users')->count();
             @endphp
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
@@ -32,7 +32,7 @@
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Jakarta Pusat</div>
+                                    Data Users</div>
                                 <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $total }}</div>
                             </div>
                             <div class="col-auto">
